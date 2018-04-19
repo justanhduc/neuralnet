5994ed80eeae78c5a91310ed1ca7650390dc9521
anhduc1210 <anhduc12101992@gmail.com>
2018-04-19 23:54:02 +0900

    minor changes

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='neuralnet',
-    version='0.0.1',
+    version='0.0.2',
     description='A high-level library on top of Theano.',
     long_description=read('readme.md'),
     long_description_content_type='text/markdown',
