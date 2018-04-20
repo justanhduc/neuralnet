6c5a2d886f802c382240e575ffd97e333d83231d
anhduc1210 <anhduc12101992@gmail.com>
2018-04-20 11:29:51 +0900

    minor changes

--- setup.py
+++ setup.py
@@ -1,37 +1,29 @@
 from setuptools import setup, find_packages
-import codecs
 import os
 
 
 here = os.path.abspath(os.path.dirname(__file__))
-
-
-def read(*parts):
-    with codecs.open(os.path.join(here, *parts), "rb", "utf-8") as f:
-        return f.read()
-
-
 with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-
 setup(
     name='neuralnet',
-    version='0.0.2',
+    version='0.0.3a',
     description='A high-level library on top of Theano.',
-    long_description=read('readme.md'),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/justanhduc/neuralnet',
     author='Duc Nguyen',
     author_email='adnguyen@yonsei.ac.kr',
     classifiers=[
         'Development Status :: 3 - Alpha',
-        'Intended Audience :: Practitioners',
-        'License :: OSI Approved :: Mozilla Public License 2.0',
-        'Programming Language :: Python :: 3.4',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Operating System :: Microsoft :: Windows :: Windows 10',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.5'
     ],
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    packages=find_packages(exclude=['examples']),
     install_requires=['theano==0.9.0', 'matplotlib', 'scipy', 'numpy'],
     project_urls={
         'Bug Reports': 'https://github.com/justanhduc/neuralnet/issues',
