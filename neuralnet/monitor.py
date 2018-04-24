d178e49bebefa622337e67c13d279359a35c4ce1
anhduc1210 <anhduc12101992@gmail.com>
2018-04-24 16:15:33 +0900

    minor changes

--- neuralnet/monitor.py
+++ neuralnet/monitor.py
@@ -19,11 +19,11 @@ from neuralnet import utils, model
 class Monitor(utils.ConfigParser):
     def __init__(self, config_file):
         super(Monitor, self).__init__(config_file)
+        self.__num_since_beginning = collections.defaultdict(lambda: {})
+        self.__num_since_last_flush = collections.defaultdict(lambda: {})
+        self.__img_since_last_flush = collections.defaultdict(lambda: {})
+        self.__iter = [0]
         self.name = self.config['model']['name']
-        self._num_since_beginning = collections.defaultdict(lambda: {})
-        self._num_since_last_flush = collections.defaultdict(lambda: {})
-        self._img_since_last_flush = collections.defaultdict(lambda: {})
-        self._iter = [0]
         self.root = 'results' if self.config['result']['root'] is None else self.config['result']['root']
         self.path = self.root + '/' + self.name
         if not os.path.exists(self.root):
@@ -42,37 +42,37 @@ class Monitor(utils.ConfigParser):
         print('Result folder: %s' % self.current_folder)
 
     def dump_model(self, network):
-        assert isinstance(network, model.Model), 'network must be an instance of Model, got {}'.format(type(network))
+        assert isinstance(network, model.Model), 'network must be an instance of Model, got {}.'.format(type(network))
         with open('%s/network.txt' % self.current_folder, 'w') as outfile:
             outfile.write("\n".join(str(x) for x in network))
 
     def tick(self):
-        self._iter[0] += 1
+        self.__iter[0] += 1
 
     def plot(self, name, value):
-        self._num_since_last_flush[name][self._iter[0]] = value
+        self.__num_since_last_flush[name][self.__iter[0]] = value
 
     def save_image(self, name, tensor_img, callback=None):
-        self._img_since_last_flush[name][self._iter[0]] = tensor_img if callback is None else callback(tensor_img)
+        self.__img_since_last_flush[name][self.__iter[0]] = tensor_img if callback is None else callback(tensor_img)
 
     def flush(self):
         prints = []
 
-        for name, vals in list(self._num_since_last_flush.items()):
+        for name, vals in list(self.__num_since_last_flush.items()):
             prints.append("{}\t{}".format(name, np.mean(list(vals.values()))))
-            self._num_since_beginning[name].update(vals)
+            self.__num_since_beginning[name].update(vals)
 
-            x_vals = np.sort(list(self._num_since_beginning[name].keys()))
-            y_vals = [self._num_since_beginning[name][x] for x in x_vals]
+            x_vals = np.sort(list(self.__num_since_beginning[name].keys()))
+            y_vals = [self.__num_since_beginning[name][x] for x in x_vals]
 
             plt.clf()
             plt.plot(x_vals, y_vals)
             plt.xlabel('iteration')
             plt.ylabel(name)
             plt.savefig(self.current_folder + '/' + name.replace(' ', '_')+'.jpg')
-        self._num_since_last_flush.clear()
+        self.__num_since_last_flush.clear()
 
-        for name, vals in list(self._img_since_last_flush.items()):
+        for name, vals in list(self.__img_since_last_flush.items()):
             for val in vals.values():
                 if val.dtype != 'uint8':
                     val = (255.99 * val).astype('uint8')
@@ -85,12 +85,15 @@ class Monitor(utils.ConfigParser):
                     imsave(name + '.jpg', val)
                 else:
                     raise NotImplementedError
-        self._img_since_last_flush.clear()
+        self.__img_since_last_flush.clear()
 
         with open(self.current_folder + '/log.pkl', 'wb') as f:
-            pickle.dump(dict(self._num_since_beginning), f, pickle.HIGHEST_PROTOCOL)
+            pickle.dump(dict(self.__num_since_beginning), f, pickle.HIGHEST_PROTOCOL)
 
-        print("Iteration {}\t{}".format(self._iter[0], "\t".join(prints)))
+        print("Iteration {}\t{}".format(self.__iter[0], "\t".join(prints)))
 
-    def read_log(self):
-        raise NotImplemented
+    def reset(self):
+        self.__num_since_beginning = collections.defaultdict(lambda: {})
+        self.__num_since_last_flush = collections.defaultdict(lambda: {})
+        self.__img_since_last_flush = collections.defaultdict(lambda: {})
+        self.__iter = [0]
