c73c949d74f891064ea26e99cc186f7e648aba32
Duc Nguyen <adnguyen@yonsei.ac.kr>
2018-08-04 17:36:02 +0900

    minor changes
    Signed-off-by: Duc Nguyen <adnguyen@yonsei.ac.kr>

--- neuralnet/__init__.py
+++ neuralnet/__init__.py
@@ -8,6 +8,7 @@ from .init import *
 from .extras import *
 from .utils import *
 from . import read_data
+from . import transforms
 from .build_training import Training
 from .build_optimization import Optimization
 from .model import Model
